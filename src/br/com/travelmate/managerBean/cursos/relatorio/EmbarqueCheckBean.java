f25940f9f55a988a36c7c63ba60f682243b2e9bb
wolverine <wolverine@192.168.1.8>
2017-09-26 16:03:55 -0300

    novos relatorios

---
+++ src/br/com/travelmate/managerBean/cursos/relatorio/EmbarqueCheckBean.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.travelmate.managerBean.cursos.relatorio;
+
+/**
+ *
+ * @author wolverine
+ */
+public class EmbarqueCheckBean {
+    
+    private String nome;
+    private String escola;
+    private String cidade;
+    private String pais;
+    private String dataInicio;
+    private String curso;
+    private String acomodacao;
+    private String visto;
+    private String seguro;
+    private String transfer;
+    private String kitViagem;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEscola() {
+        return escola;
+    }
+
+    public void setEscola(String escola) {
+        this.escola = escola;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getAcomodacao() {
+        return acomodacao;
+    }
+
+    public void setAcomodacao(String acomodacao) {
+        this.acomodacao = acomodacao;
+    }
+
+    public String getVisto() {
+        return visto;
+    }
+
+    public void setVisto(String visto) {
+        this.visto = visto;
+    }
+
+    public String getSeguro() {
+        return seguro;
+    }
+
+    public void setSeguro(String seguro) {
+        this.seguro = seguro;
+    }
+
+    public String getTransfer() {
+        return transfer;
+    }
+
+    public void setTransfer(String transfer) {
+        this.transfer = transfer;
+    }
+
+    public String getKitViagem() {
+        return kitViagem;
+    }
+
+    public void setKitViagem(String kitViagem) {
+        this.kitViagem = kitViagem;
+    }
+    
+    
+    
+}
--- src/br/com/travelmate/managerBean/cursos/relatorio/EmbarqueCheckFactory.java
+++ src/br/com/travelmate/managerBean/cursos/relatorio/EmbarqueCheckFactory.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.travelmate.managerBean.cursos;
+package br.com.travelmate.managerBean.cursos.relatorio;
 
 import java.util.List;
 
